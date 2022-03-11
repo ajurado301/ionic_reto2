e24ebce79890033425b16efcef39e907d3bb3247
ajurado301 <ajurado301@gmail.com>
2022-03-11 18:56:03 +0100

    Reto 2 pendiente de corregir

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation } from "@ionic-native/geolocation/ngx"
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,40 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public latPro: number;
+  public lonPro: number;
+  public latObs: number;
+  public lonObs: number;
 
+  constructor(private geolocation: Geolocation) {
+    this.reset();
+  }
+
+  public gpsPromesa(){
+    this.geolocation.getCurrentPosition()
+    .then((resp)=>{
+      this.latPro = resp.coords.latitude;
+      this.lonPro = resp.coords.longitude;
+    })
+    .catch((error)=> {
+      console.log("Error al geolocalizar con promesa", error);
+    })
+  }
+
+  public gpsObservable(){
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((resp: any)=>{
+      this.latObs = resp.coords.latitude;
+      this.lonObs = resp.coords.longitude;
+    }, (error) => {
+      console.log("Error al geolocalizar con observable", error);
+    })
+  }
+
+  reset() {
+    this.latPro = 0;
+    this.lonPro = 0;
+    this.latObs = 0;
+    this.lonObs = 0;
+  }
 }
